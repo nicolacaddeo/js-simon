0834cbba96e44d2963cb7094d84933f8bb818f67
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-09-05 23:50:26 +0200

    created function to display numbers

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,27 @@
 // sul click del bottone vengono generati 5 numeri
 
 const playBtn = document.getElementById('init-btn');
-const randomNumbers = [];
+const squareContainer = document.getElementById('squares-container');
+let randomNumbers = [];
 
 playBtn.addEventListener('click',
     function () {
+        squareContainer.innerHTML = '';
+
+        // ciclo per creare array con 5 numeri random
         for (let index = 0; index < 5; index++) {
             let number = Math.floor(Math.random() * 100);
             randomNumbers.push(number);
             console.log(number);
             console.log(randomNumbers);
+
+            // mando a schermo i numeri
+            const square = document.createElement('div');
+            square.className = 'nc-num-container';
+            square.innerHTML = number;
+            squareContainer.append(square);
         }
     }
 );
+
+
